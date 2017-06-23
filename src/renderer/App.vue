1dcae4ac369e2b7bc32aa90acac839aee0c83caa
LeoHeo <hjh5488@gmail.com>
2017-06-23 12:18:15 +0900

    Test

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test-electron22'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
